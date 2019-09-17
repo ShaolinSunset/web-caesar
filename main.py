bcf94a8ba74313ccef0bd099f309004a6f043258
Sunset Snell <anicia.snell.as@gmail.com>
2019-09-17 19:03:26 -0400

    added html file to global variable form

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from flask import Flask
+import cgi
+import os
+import jinja2
+
+template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+form = """<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {
+                backgroud-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width:540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+        <form action="text" method = "POST">
+            <label for="">LABEL NAME:</label>
+            <input id="" type="text" name="rot"/>
+            <textarea id="" type="text" name="text"/>
+            <input type="submit" />
+        </form>
+    </body>
+</html>"""
+
 
 @app.route("/")
 def index():
